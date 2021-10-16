1d5e7487d35c9bd6419d04af3b23807d2d5892cf
carlosbarondev <carlosbarondev@gmail.com>
2021-10-16 13:54:17 +0200

    Añadido archivo de variables de entorno

--- models/busquedas.js
+++ models/busquedas.js
@@ -8,10 +8,28 @@ class Busquedas {
 
     }
 
-    async ciudad(lugar = '') {
-        console.log(lugar);
+    get paramsMapbox() {
+        return {
+            'access_token': process.env.MAPBOX_KEY,
+            'limit': 5,
+            'language': 'es'
+        }
+    }
 
-        return [];
+    async ciudad(lugar = '') {
+        try {
+            // Peticion http
+            const instance = axios.create({
+                baseURL: `https://api.mapbox.com/geocoding/v5/mapbox.places/${lugar}.json`,
+                params: this.paramsMapbox,
+            });
+            const resp = await instance.get();
+            //const resp = await axios.get("https://api.mapbox.com/geocoding/v5/mapbox.places/hue.json?language=es&access_token=pk.eyJ1IjoiY2FybG9zYnAiLCJhIjoiY2t1dHBnMjl6MG1keDJucDdlZ3lod3NrciJ9.kxFNBi8Jln1pLAP6ZLhycQ");
+            console.log(resp.data);
+            return [];
+        } catch (error) {
+            return [];
+        }
     }
 }
 
